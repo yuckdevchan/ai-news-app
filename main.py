6e08227e3fe58fb2ef88218fa5148c7691f59137
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2024-10-23 00:29:01 +0100

    Stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import news
 
 app = Flask(__name__)
@@ -11,6 +11,14 @@ settings = {
 def index():
     topic = "Headlines"
     requested_news = news.get_news(topic)
+    return render_template("home.html", name=settings["name"], news=requested_news, topic=topic, topic_color=news.topic_id[topic][1])
+
+@app.route("/topic/<topic>")
+def topic(topic):
+    topic = topic.capitalize()
+    requested_news = news.get_news(topic)
+    if requested_news == ["Invalid topic."]:
+        return redirect("/")
     return render_template("home.html", name=settings["name"], news=requested_news, topic=topic, topic_color=news.topic_id[topic][1])
 
 if __name__ == "__main__":
--- news.py
+++ news.py
@@ -1,16 +1,23 @@
 import requests
+from pathlib import Path
 from GoogleNews import GoogleNews
 googlenews = GoogleNews(lang="en")
 
 topic_id = {
     "Headlines": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSFFpZ0FQAQ", "#8BB8F8"),
-    "World": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx1YlY4U0JXVnVMVWRDR2dKSFFpZ0FQAQ"),
-    "Technology": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKSFFpZ0FQAQ"),
-    "Entertainment": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNREpxYW5RU0JXVnVMVWRDR2dKSFFpZ0FQAQ")
+    "World": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx1YlY4U0JXVnVMVWRDR2dKSFFpZ0FQAQ", "#689F38"),
+    "Technology": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKSFFpZ0FQAQ", "#039BE5"),
+    "Entertainment": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNREpxYW5RU0JXVnVMVWRDR2dKSFFpZ0FQAQ", "#7E57C2"),
+    "Sports": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKSFFpZ0FQAQ", "#EF6C00"),
+    "Science": ("CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp0Y1RjU0JXVnVMVWRDR2dKSFFpZ0FQAQ", "#E91E63"),
+    "Health": ("CAAqJQgKIh9DQkFTRVFvSUwyMHZNR3QwTlRFU0JXVnVMVWRDS0FBUAE", "#5677FC"),
 }
 
 def get_news(topic: str) -> list:
-    googlenews.set_topic(topic_id[topic][0])
+    try:
+        googlenews.set_topic(topic_id[topic][0])
+    except KeyError:
+        return ["Invalid topic."]
     googlenews.get_news()
     results = googlenews.results()[:30]
     for story in results:
@@ -18,4 +25,9 @@ def get_news(topic: str) -> list:
             img = story["img"]
             img = requests.get(img).url
             story["img"] = img
+        if Path(f"static/media/{story['media']}.png").exists():
+            story["mediaFavicon"] = f"/static/media/{story['media']}.png"
+        else:
+            story["mediaFavicon"] = None
+    googlenews.clear()
     return results
